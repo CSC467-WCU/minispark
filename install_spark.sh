d0ca54c5dce7cacc216d4613e3181286c9ef507c
Linh B Ngo <lngo@wcupa.edu>
2021-09-05 21:15:01 -0400

    Update install_spark.sh

--- install_spark.sh
+++ install_spark.sh
@@ -12,7 +12,7 @@ set -x
 
 apt-get update && apt-get install -qqq -y curl vim wget software-properties-common ssh net-tools ca-certificates jq
 apt-get install -qqq -y python3 python3-pip python3-numpy python3-matplotlib python3-scipy python3-pandas python3-simpy ipython3
-pip install -y ipykernel jupyter findspark
+pip3 install -y ipykernel jupyter findspark
 
 cd /opt
 wget https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.12%2B7/OpenJDK11U-jdk_x64_linux_hotspot_11.0.12_7.tar.gz
